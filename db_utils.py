6a23aee9e36f9ce22f1961bd6b5fdeea7bae1ac1
Bill Pratt <pratt.william7@gmail.com>
2020-04-05 19:28:08 +0000

    added option database insertion method

--- db_utils.py
+++ db_utils.py
@@ -30,3 +30,27 @@ def resolve_symbols(df_scrape):
     conn.close()
 
     return len(df_diff.index)
+
+def insert_options(df_options, rec_date):
+
+    conn = pymysql.connect(host=MYSQL_HN, db=MYSQL_DB, user=MYSQL_UN, password=MYSQL_PW)
+    cur = conn.cursor()
+
+    rec_date = rec_date.strftime('%Y-%m-%d')
+
+    for i in range(len(df_options.index)):
+        contract = df_options['contract'].iloc[i]
+        symbol = df_options['symbol'].iloc[i]
+        type_ = str(df_options['type'].iloc[i])
+        strike = str(df_options['strike'].iloc[i])
+        exp_date = df_options['exp_date'].iloc[i].strftime('%Y-%m-%d')
+        bid = str(df_options['bid'].iloc[i])
+        ask = str(df_options['ask'].iloc[i])
+        volume = str(df_options['volume'].iloc[i])
+
+        cmd = 'insert into option_data values ("' + contract + '","' + rec_date + '","' + symbol + '",'
+        cmd = cmd + type_ + ',' + strike + ',"' + exp_date + '",' + bid + ',' + ask + ',' + volume + ',NULL);'
+        cur.execute(cmd)
+
+    conn.commit()
+    conn.close()
